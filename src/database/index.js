e021a4f555c4d0b605ebad5ecf1bf4a1e679ebfe
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-31 17:16:59 -0300

    School entity added to project

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize')
 const dbConfig = require('../config/database')
 const Donator = require('../models/Donator')
+const School = require('../models/School')
 
 const connection = new Sequelize(dbConfig)
 
 Donator.init(connection)
+School.init(connection)
 
 module.exports = connection
