e9822f3dd944f85397d03dc4a2fb386c9ed63959
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-31 14:23:18 -0300

    App configs

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+	dialect: 'mysql',
+	host: '92.249.45.2',
+	username: 'u264151736_konectando',
+	password: 'spaceapps',
+	database: 'u264151736_konectando',
+	define: {
+		timestamps: true,
+		underscored: true
+	}
+}
