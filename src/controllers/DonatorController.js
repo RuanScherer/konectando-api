b3446f48582f456a65ee8f83873c0a6e9d751aa6
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-31 16:01:10 -0300

    Register donators done

---
+++ src/controllers/DonatorController.js
@@ -0,0 +1,46 @@
+const Donator = require('../models/Donator')
+const { Op } = require('sequelize')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth.json')
+const bcrypt = require('bcryptjs')
+
+function clearSensitiveData(model) {
+	model.password = undefined
+}
+
+function generateToken(params) {
+	return jwt.sign(params, authConfig.appHash, {
+		expiresIn: 86400
+	})
+}
+
+module.exports = {
+	async store(req, res) {
+		const { name, email, phone, password } = req.body
+
+		if (password.length < 8 || phone.length < 10) return res.status(400).send()
+
+		const donator = await Donator.findOne({
+			where: {
+				[Op.or]: [
+					{ name: name },
+					{ email: email }
+				]
+			}
+		})
+
+		if (donator) return res.status(400).send()
+
+		await Donator.create({ name, email, phone, password })
+			.then((donator) => {
+				clearSensitiveData(donator)
+				return res.json({
+					donator,
+					token: generateToken({ id: donator.id })
+				})
+			})
+			.catch(() => {
+				return res.status(500).send()
+			})
+	}
+}
