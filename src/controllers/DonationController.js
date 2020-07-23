eb3e34c9c503554b07bc999e487e2729415fcfee
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-23 13:14:48 -0300

    List donations

--- src/controllers/DonationController.js
+++ src/controllers/DonationController.js
@@ -2,6 +2,18 @@ const Donation = require("../models/Donation");
 const School = require("../models/School");
 const Donator = require("../models/Donator");
 
+function clearSensitiveData(array, type) {
+	return newArray = array.map(element => {
+		if (type === "receptor") {
+			element.receptor.password = undefined
+		}
+		else if (type === "donator") {
+			element.donator.password = undefined
+		}
+		return element
+	})
+}
+
 module.exports = {
 	async store(req, res) {
 		const { userId } = req
@@ -23,5 +35,41 @@ module.exports = {
 
 		if (!donation) return res.status(500).send({ err: "Server error" })
 		return res.send({ donation })
+	},
+
+	async listByDonator(req, res) {
+		const { userId } = req
+
+		let donations = await Donation.findAll({
+			where: {
+				donator_id: userId
+			},
+			include: {
+				association: "receptor"
+			}
+		})
+
+		donations = clearSensitiveData(donations, "receptor")
+
+		if (!donations) return res.status(500).send({ err: "Server error" })
+		return res.send({ donations })
+	},
+
+	async listBySchool(req, res) {
+		const { userId } = req
+
+		let donations = await Donation.findAll({
+			where: {
+				school_id: userId
+			},
+			include: {
+				association: "donator"
+			}
+		})
+
+		donations = clearSensitiveData(donations, "donator")
+
+		if (!donations) return res.status(500).send({ err: "Server error" })
+		return res.send({ donations })
 	}
 }
